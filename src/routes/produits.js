275749a56343c5c107d01a87adefa94697ddaa2c
LudovicRocher01 <ludovicrocher01@yahoo.fr>
2025-03-06 00:55:05 +0100

    Mise à jour du site en production

--- src/routes/produits.js
+++ src/routes/produits.js
@@ -8,7 +8,7 @@ const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
     windowMs: 10 * 60 * 1000,
-    max: 20,
+    max: 100,
     message: { error: "Trop de requêtes, veuillez réessayer plus tard." }
 });
 
